cb1145b64d208d6fe7cc353657b2d357128f2af4
chand1012 <chandler@multisiglabs.org>
2023-04-10 20:58:49 -0400

    Switch to bearer token

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 import secrets
@@ -12,37 +12,53 @@ class Summary(BaseModel):
 
 load_dotenv()
 
-BASIC_AUTH_USER = os.getenv("BASIC_AUTH_USER")
-BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_URL = os.getenv("DB_URL", "http://127.0.0.1:8090")
+
+if not AUTH_TOKEN:
+    AUTH_TOKEN = secrets.token_urlsafe(32)
+    print("Generated new auth token:", AUTH_TOKEN)
+
+# define the middleware function
+async def token_middleware(request: Request, call_next):
+    # ensure the header is present and formatted correctly
+    header_dict = request.headers
+    authorization = header_dict.get('Authorization')
+    if not authorization or not authorization.startswith('Bearer '):
+        raise HTTPException(status_code=401, detail='Authorization header invalid')
+
+    # extract the token from the header
+    token = authorization.split(" ")[1]
+
+    # validate the token here
+    if token != AUTH_TOKEN:
+        raise HTTPException(status_code=401, detail='Invalid token')
+
+    # token is valid, pass it to the route handler
+    response = await call_next(request)
+    return response
+
 
 app = FastAPI()
 security = HTTPBasic()
 db = DB(DB_URL)
 db.login(DB_USERNAME, DB_PASSWORD)
 
-def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
-    user = secrets.compare_digest(credentials.username, BASIC_AUTH_USER)
-    password = secrets.compare_digest(credentials.password, BASIC_AUTH_PASSWORD)
-    if not (user and password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
+# register the middleware function
+app.middleware('http')(token_middleware)
 
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/transcript/{videoId}", dependencies=[Depends(authenticate)])
+@app.get("/transcript/{videoId}")
 async def transcript(videoId: str):
     transcript = db.get_transcript(videoId)
     return transcript
 
-@app.post("/summary/{videoId}", dependencies=[Depends(authenticate)])
+@app.post("/summary/{videoId}")
 async def post_summary(videoId: str, req: Summary):
     resp = db.post_summary(videoId, req.summary)
     return resp
